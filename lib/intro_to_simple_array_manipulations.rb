1b4d3ee59f64c84734f71466494f0ff8bf34a4db
Nikoloz Chapichadze <nikoloz.chapichadze@gmail.com>
2020-10-10 01:13:31 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,13 @@
+def using_concat(array1,array2)
+  array1.concat(array2)
+end 
+
+def using_insert(array,element)
+  array.insert(element,3)
+end 
+
+def usig_uniq(array)
+  array.uniq 
+end 
+
+def 
